5b5c817b261719f77788b0757cfadfa2fcaa9c31
Robert Schwartz <bschwartz23@gmail.com>
2021-04-04 16:05:23 -0700

    style header

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var questions = [
+    {
+        prompt: "What is blue?, (A) Red,  (B) Blue, (C) Green, (D) Salmon",
+        answer : 'B'
+    },
+    {
+        prompt: "What is Mika? (A) Red /n (B) Dog /n (C) Green /n (D) Salmon",
+        answer : 'C'
+    },
+    {
+        prompt: "What is Love?  (A) Baby don't hurt me /n (B) Blue /n (C) Green /n (D) Salmon",
+        answer : 'A'
+    },
+];
+var Score = 0;
