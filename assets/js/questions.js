e47d2e67a2b541a57fd55f544d1264b7ae8c4e99
Robert Schwartz <bschwartz23@gmail.com>
2021-04-04 22:05:30 -0700

    initialize questions arrays

---
+++ assets/js/questions.js
@@ -0,0 +1,90 @@
+var questionAnswer = [{
+    question: "Who am I?"
+    choice1:  "I don't Know",
+    choice2:  "I don't Know",
+    choice3:  "I don't Know",
+    choice4:  "I don't Know",
+    answer: 3
+},
+{
+    question: "Who am I 2?"
+    choice1:  "I don't Know",
+    choice2:  "I don't Know",
+    choice3:  "I don't Know",
+    choice4:  "I don't Know",
+    answer: 3
+},
+{
+    question: "Who am I 3?"
+    choice1:  "I don't Know",
+    choice2:  "I don't Know",
+    choice3:  "I don't Know",
+    choice4:  "I don't Know",
+    answer: 3
+},
+{
+    question: "Who am I 4?"
+    choice1:  "I don't Know",
+    choice2:  "I don't Know",
+    choice3:  "I don't Know",
+    choice4:  "I don't Know",
+    answer: 3
+},
+{
+    question: "Who am I 5?"
+    choice1:  "I don't Know",
+    choice2:  "I don't Know",
+    choice3:  "I don't Know",
+    choice4:  "I don't Know",
+    answer: 3
+},
+{
+    question: "Who am I 6?"
+    choice1:  "I don't Know",
+    choice2:  "I don't Know",
+    choice3:  "I don't Know",
+    choice4:  "I don't Know",
+    answer: 3
+},
+{
+    question: "Who am I 7?"
+    choice1:  "I don't Know",
+    choice2:  "I don't Know",
+    choice3:  "I don't Know",
+    choice4:  "I don't Know",
+    answer: 3
+},
+{
+    question: "Who am I 8?"
+    choice1:  "I don't Know",
+    choice2:  "I don't Know",
+    choice3:  "I don't Know",
+    choice4:  "I don't Know",
+    answer: 3
+},
+{
+    question: "Who am I?"
+    choice1:  "I don't Know",
+    choice2:  "I don't Know",
+    choice3:  "I don't Know",
+    choice4:  "I don't Know",
+    answer: 3
+},
+{
+    question: "Who am I 9?"
+    choice1:  "I don't Know",
+    choice2:  "I don't Know",
+    choice3:  "I don't Know",
+    choice4:  "I don't Know",
+    answer: 3
+},
+{
+    question: "Who am I 10?"
+    choice1:  "I don't Know",
+    choice2:  "I don't Know",
+    choice3:  "I don't Know",
+    choice4:  "I don't Know",
+    answer: 3
+},
+
+]
