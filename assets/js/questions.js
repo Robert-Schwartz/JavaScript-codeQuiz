8cf021b125ebf07a8e98b8ddb634ca97c9287a36
Robert Schwartz <bschwartz23@gmail.com>
2021-04-05 19:32:21 -0700

    day end

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,89 +1,10 @@
-var questionAnswer = [{
+var questionAnswer = [
+    {
     question: "Who am I?",
-    choice1:  "I don't Know",
-    choice2:  "I don't Know",
-    choice3:  "I don't Know",
-    choice4:  "I don't Know",
-    answer: 3
+    answers: [
+        {text: "I don't know", response: true},
+        {text: "I don't know", response: False},
+        {text: "I don't know", response: False},
+        {text: "I don't know", response: False} 
+    ]
 },
-{
-    question: "Who am I 2?",
-    choice1:  "I don't Know",
-    choice2:  "I don't Know",
-    choice3:  "I don't Know",
-    choice4:  "I don't Know",
-    answer: 3
-},
-{
-    question: "Who am I 3?",
-    choice1:  "I don't Know",
-    choice2:  "I don't Know",
-    choice3:  "I don't Know",
-    choice4:  "I don't Know",
-    answer: 3
-},
-{
-    question: "Who am I 4?",
-    choice1:  "I don't Know",
-    choice2:  "I don't Know",
-    choice3:  "I don't Know",
-    choice4:  "I don't Know",
-    answer: 3
-},
-{
-    question: "Who am I 5?",
-    choice1:  "I don't Know",
-    choice2:  "I don't Know",
-    choice3:  "I don't Know",
-    choice4:  "I don't Know",
-    answer: 3
-},
-{
-    question: "Who am I 6?",
-    choice1:  "I don't Know",
-    choice2:  "I don't Know",
-    choice3:  "I don't Know",
-    choice4:  "I don't Know",
-    answer: 3
-},
-{
-    question: "Who am I 7?",
-    choice1:  "I don't Know",
-    choice2:  "I don't Know",
-    choice3:  "I don't Know",
-    choice4:  "I don't Know",
-    answer: 3
-},
-{
-    question: "Who am I 8?",
-    choice1:  "I don't Know",
-    choice2:  "I don't Know",
-    choice3:  "I don't Know",
-    choice4:  "I don't Know",
-    answer: 3
-},
-{
-    question: "Who am I?",
-    choice1:  "I don't Know",
-    choice2:  "I don't Know",
-    choice3:  "I don't Know",
-    choice4:  "I don't Know",
-    answer: 3
-},
-{
-    question: "Who am I 9?",
-    choice1:  "I don't Know",
-    choice2:  "I don't Know",
-    choice3:  "I don't Know",
-    choice4:  "I don't Know",
-    answer: 3
-},
-{
-    question: "Who am I 10?",
-    choice1:  "I don't Know",
-    choice2:  "I don't Know",
-    choice3:  "I don't Know",
-    choice4:  "I don't Know",
-    answer: 3
-},
-]
---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+//global variables
+
+
+
+
+//Start the Game and Timer
+function gameStart (){
+ 
+
+}
+
+//continue to next question
+function gameNext () {
+
+}
+
+//Choose answer
+function chooseAnswer () {
+
+}
+
+//Display result, add score
+
+
+
+//next or quit
+
+
+//end game
+
+
+//display score
+
+
+
+
+//questions list:
+var questionCard = [
+    {
+    question: "Who am I?",
+    answers: [
+        {text: "I don't know", response: true},
+        {text: "I don't know", response: False},
+        {text: "I don't know", response: False},
+        {text: "I don't know", response: False} 
+    ]
+},
